0d7d5c117b7f51c7c5385ac969d7edcad840bd58
maheshgoud49 <maheshgoud49@gmail.com>
2023-07-22 19:54:10 +0530

    updating test file to validate new branch

--- Git/src/main/java/org/example/file2.java
+++ Git/src/main/java/org/example/file2.java
@@ -4,7 +4,7 @@ public class file2 {
     public static void main(String[] args)
     {
         System.out.println("Am from file-2");
-        System.out.println("updated again");
+        System.out.println("updated as part of new branch validation");
 
     }
 }
